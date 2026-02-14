dafde18e18836289f8548305d3516c85fd193273
prit2205 <pritpatel2500@gmail.com>
2026-02-14 17:10:36 +0530

    update code

---
+++ dom.js
@@ -0,0 +1,22 @@
+// DOM -- Document Object Model
+// All Website Code --> DOM
+// Website Output --> DOM
+// DOM -- HTML body
+
+// DOM Tree Structure:
+// Node --> Element (ex. h1, p, b, i), text (ex. lorem), comment
+
+// DOM Manipulation - change body
+// select element from html
+// chagne text 
+// change html 
+// change css 
+// change attribute 
+// event listeners
+
+// Selecting Elements
+// getElementById
+let a = document.getElementById("abc")
+console.log(a);
+
+// getElementsByClassName
